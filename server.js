5efc654e55d29b3ef77a4922e6b1b63f609ba76b
robmc8854 <robmcaloon@yahoo.co.uk>
2025-08-13 19:07:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const DIST = path.join(__dirname, 'dist');
 console.log('[boot] Node:', process.version);
 console.log('[boot] PORT:', PORT);
 console.log('[boot] dist exists?', fs.existsSync(DIST));
+console.log('[boot] index.html exists?', fs.existsSync(path.join(DIST, 'index.html')));
 
 app.get('/healthz', (_req, res) => {
   res.json({
@@ -30,7 +31,9 @@ if (fs.existsSync(DIST)) {
   app.use(express.static(DIST, { index: false, maxAge: '1h' }));
   app.get('*', (_req, res) => res.sendFile(path.join(DIST, 'index.html')));
 } else {
-  app.get('*', (_req, res) => res.status(200).send('<pre>dist/ not found. Did the build run?</pre>'));
+  app.get('*', (_req, res) =>
+    res.status(200).send('<pre>dist/ not found. Did the build run?</pre>')
+  );
 }
 
 app.listen(PORT, '0.0.0.0', () => {
